a828d217b1f5d7bbe182d04174cdffe49329270e
crisilva123 <crisilva123@gmail.com>
2026-02-05 20:22:32 -0300

    Add files via upload

---
+++ Maior_Mensor_igual.py
@@ -0,0 +1,29 @@
+x = "ab"
+y = "ac"
+
+print(x >y)
+print(x < y)
+
+
+len(x)
+len(y)
+print(len(x))
+print(len(x) <= len(y))
+print(len(x) != len(y))
+
+
+texto1 = len("aabb")
+texto2 = len("bbacc")
+
+print(texto1 >= texto2)
+print(texto1 == texto2)
+print(texto1 < texto2)
+print(texto1 > texto2)
+print(texto1 <= texto2)
+print(texto1!= texto2)
+
+a = len("aab")
+b = len("bbacc")
+
+print(a >= b)
+print(a == b)
---
+++ bool.py
@@ -0,0 +1,31 @@
+print(2==3)
+print(7==7)
+print(5!=2)
+
+x = 8
+y = 5
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+soma = input("somar")
+subtrair = input("subtrair")
+
+print(soma > subtrair)
+print(soma < subtrair)
+print(soma == subtrair)
+print(soma != subtrair)
+print(soma >= subtrair)
+print(soma <= subtrair)
+
+
+print(subtrair < soma)
+print(subtrair > soma)
+
+
+
+
+
